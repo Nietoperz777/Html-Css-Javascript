ed4ad13f3d8a899d446f088834cd5d8f717a24d9
Grzesiu Kwiatkowski <u.grzegorz.kwiatkowski@adegdansk.pl>
2025-05-03 11:38:44 +0200

    Add files via upload

---
+++ filmy.js
@@ -0,0 +1,7 @@
+filmy = ["Sonic", "Sonic 2", "Sonic 3", "Super Mario Bros" , "Lego: Przygoda"]
+
+print("Aktualna wielkość kolekcji:", length(filmy))
+
+print("Dwa pierwsze filmy:", filmy[0], filmy[1])
+
+print("Ostatni film:", filmy[-1])
